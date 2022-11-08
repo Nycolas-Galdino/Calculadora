1e0fb3b17bc7bf3796a664f1b73112eb300ddf8b
Nycolas Pimentel Galdino <nycolaspimentel12@gmail.com>
2022-11-08 14:42:24 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function result(num){};
+
+function getBtn(btn){
+    const resultado = document.getElementById("resultado")
+    resultado.innerHTML = resultado + btn
+};
+
+function clear(){};
+
+function OnOff(){};
