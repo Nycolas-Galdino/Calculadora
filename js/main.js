b755d727241d1d7de35310c2321ecf9c771ceaa7
Nycolas Pimentel Galdino <nycolaspimentel12@gmail.com>
2022-11-08 16:43:28 -0300

    Projeto: Calculadora Versão: 1.3 Data de atualiação: 08/11/2022 16:44
    * Adicionado:
    - Integração com o teclado;
    - Formatação do rodapé.
    
    * A implementar ainda:
    - Integração do projeto com o GitHub Pages.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+document.addEventListener('keypress',teclado)
+
 function result(num){
-    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada")}
+    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada \n Pressione 'o' para ligar a mesma.")}
 
     let resultado = document.getElementById("resultado").innerHTML
     document.getElementById("resultado").innerHTML = eval(resultado)
 };
 
 function getBtn(btn){
-    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada")}
+    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada \n Pressione 'o' para ligar a mesma.")}
 
     let resultado = document.getElementById("resultado")
     let texto = document.getElementById("resultado").innerHTML
@@ -15,7 +17,6 @@ function getBtn(btn){
 
 function clean(){
         document.getElementById("resultado").innerHTML = ""
-        console.log("limpo")
 };
 
 function OnOff(){
@@ -30,3 +31,24 @@ function OnOff(){
         clean()
     }
 };
+
+function teclado(event){
+    var key = event.keyCode || event.which;
+    let keypressed = String.fromCodePoint(key); 
+
+        console.log(keypressed)
+    if (keypressed.toUpperCase() == "O") {
+        return OnOff()
+    }
+
+    if (event.key == "Enter") {
+        return result()
+    }
+
+    if ("1234567890.-*/()x".indexOf(keypressed) > -1 || keypressed == "+"){
+        console.log('ok')
+        return getBtn(keypressed)
+    }
+    
+    else (alert("Botão não aceito pela calculadora"))
+}
