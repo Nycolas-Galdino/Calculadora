ffbc0266f14c35e0ad2a88fc18f0eae6a0a62a26
Nycolas Pimentel Galdino <nycolaspimentel12@gmail.com>
2022-11-08 15:31:25 -0300

    Projeto: Calculadora Versão: 1.2 Data de atualiação: 08/11/2022 15:31
    * Adicionado:
    - Função de limpar, ligar e desligar calculadora, reconhecer teclas apertando nas mesmas.
    - Lay-Out mais responsivo
    
    * A corrigir:
    - Tamanho da caixa de resultado quando ultrapassa tamanho padrão;
    - Formatação de rodapé da página.
    
    * A implementar ainda:
    - Integração do projeto com o GitHub Pages.
    
    Ass: Nycolas Pimentel Galdino

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-function result(num){};
+function result(num){
+    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada")}
 
-function getBtn(btn){
-    const resultado = document.getElementById("resultado")
-    resultado.innerHTML = resultado + btn
+    let resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = eval(resultado)
 };
 
-function clear(){};
+function getBtn(btn){
+    if (document.getElementById("btnOnOff").innerHTML == "Off"){ return alert("A calculadora está desligada")}
 
-function OnOff(){};
+    let resultado = document.getElementById("resultado")
+    let texto = document.getElementById("resultado").innerHTML
+    resultado.innerHTML = texto + btn
+};
+
+function clean(){
+        document.getElementById("resultado").innerHTML = ""
+        console.log("limpo")
+};
+
+function OnOff(){
+    if (document.getElementById("btnOnOff").innerHTML == "Off"){
+        document.getElementById("btnOnOff").innerHTML = "On"
+        document.getElementById("btnOnOff").style.backgroundColor = "rgb(144, 238, 144)"
+    }
+
+    else if (document.getElementById("btnOnOff").innerHTML == "On"){
+        document.getElementById("btnOnOff").innerHTML = "Off"
+        document.getElementById("btnOnOff").style.backgroundColor = "rgb(240, 128, 128)"
+        clean()
+    }
+};
